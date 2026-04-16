bf247ad4b7ee6e4c5d2cc5f7442c7f36b6319fc1
Mohammed Falion <mohammedfalion@outlook.com>
2026-04-16 14:37:59 +0300

    Initial Push

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/Owais-Portfolio/' : '/',
-  plugins: [react()],
-}))
